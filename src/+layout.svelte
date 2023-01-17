ce211e4bb4e6858c4527ccfe228d5a7b30a31e5a
Daniel Bider <daniel.bider@fenergo.com>
2023-01-17 14:01:14 +0100

    Moved from tabs to spaces, added bootstrap and sveltekit static adapter

---
+++ src/+layout.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    import 'bootstrap';
+    export const prerender = true;
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let text = 'Pchnąć w tę łódź jeża lub ośm skrzyń fig';
+</script>
+
+<h1>Font Awesome Comparer</h1>
